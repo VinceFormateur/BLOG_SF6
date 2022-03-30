d1318dfdacf62cd2b32dfd75433e3f82e3e0605e
VinceFormateur <Vince@Vince.com>
2022-03-30 17:06:03 +0200

    POST Form (Part 1)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,21 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/', name: 'app_main_')]
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function home(): Response
     {
         return $this->render('main/home.html.twig');
     }
 
-    #[Route('/contact', name: 'app_contact')]
+    #[Route('/contact', name: 'contact')]
     public function contact(): Response
     {
         return $this->render('main/contact.html.twig');
-    }    
+    }
+
+    #[Route('/creer_un_article', name: 'new_post')]
+    public function newPost(): Response
+    {
+
+        $post = new Post();
+
+        $form = $this->createForm(PostFormType::class, $post);
+
+        return $this->renderForm('main/newPost.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
 }
