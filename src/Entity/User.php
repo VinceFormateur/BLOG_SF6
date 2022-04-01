59d4edc314e7c8ed18c35801dd9417d6f22dff9a
VinceFormateur <Vince@Vince.com>
2022-04-01 17:14:23 +0200

    REGISTRATION (Template + Constraints)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\HasLifecycleCallbacks]
-#[UniqueEntity(fields: ['email', 'username'], message: 'Ce compte existe déjà')]
+#[UniqueEntity(fields: ['email'], message: 'Ce compte existe déjà')]
+#[UniqueEntity(fields: ['username'], message: 'Ce pseudo existe déjà')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
