c6de7f00731904a1bc507ba87709ffaaea914f0a
VinceFormateur <Vince@Vince.com>
2022-04-01 14:47:10 +0200

    LOGIN / LOGOUT

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 #[Route('/', name: 'app_main_')]
@@ -27,5 +27,25 @@ class MainController extends AbstractController
         return $this->render('main/contact.html.twig');
     }
 
+    #[Route(path: '/connexion', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_main_home');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/deconnexion', name: 'logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
 
 }
